61a77dc64e0d9be3f0a8c74f3399efb2cb843798
Ed Campbell <drescampbell@gmail.com>
2019-03-07 20:52:53 +0000

    Change user

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import main from './main';
 import calcTotal from './dcfun';
 import plot from './dcfun_pt2';
 
-const user = { firstName: 'Foo', lastName: 'Bar' };
+const user = { firstName: 'Apple', lastName: 'Crumble' };
 
 main(user);
 calcTotal();
